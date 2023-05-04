a2ab7a4d4cfda358ab812c9fbf138a3a03620cf2
DuxaDruha <wrackled8998@gmail.com>
2023-05-04 18:47:18 +0300

    проверка коммита

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, login_required, logout_user
 
+
+# проверка коммита написал рандомный комментарий
+
+
 from data import db_session
 from data.jobs import Jobs
 from data.login_form import LoginForm
